3151b7bcb9ed8bc56cc6ea61fcedb4eab68bc9bd
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-29 11:25:38 +0300

    Решил задачу 3

---
+++ Sem_3/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+Console.Write($"{num} ->  ");
+while(i <= num){
+    
+    if(i == num){
+        Console.Write($"{i * i}");
+    }
+    else{
+        Console.Write($"{i * i}, ");
+    }
+    i++;
+}
