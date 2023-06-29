05121396a9ca285b279e47af57a6f5365090eb99
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-29 10:32:29 +0300

    Решил задачу 18

---
+++ Sem_3/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num == 1){
+    Console.WriteLine("x > 0, y > 0");
+}
+else if(num == 2){
+    Console.WriteLine("x < 0, y > 0");
+}
+else if(num == 3){
+    Console.WriteLine("x < 0, y < 0");
+}
+else if(num == 4){  
+    Console.WriteLine("x > 0, y < 0");
+
+}
+else{
+    Console.WriteLine("Такой четверти не сушествует");
+}
