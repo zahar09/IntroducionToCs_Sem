c79a83ca0843f5e915e51a5ddacd22ee828eb819
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-03 11:19:41 +0300

    Решил задачу 3

---
+++ Sem_4/task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+//int[]
+// //[1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2, 7);
+// int b = new Random().Next(2);
+PrintArr(Arr());
+
+int[] Arr(){
+    int n = 8;
+    int[] arr = new int[n];
+    for(int i = 0;i < arr.Length;i++){
+        arr[i] = new Random().Next(2);
+    }
+    return arr;
+}
+void PrintArr(int[] arr){
+    Console.Write("[");
+    for(int i = 0;i < arr.Length - 1;i++){
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
