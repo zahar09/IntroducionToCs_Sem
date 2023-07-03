82dad89c9232f92ee471bea60c65c0647ec48d1c
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-03 11:02:45 +0300

    Решил задачу 2

---
+++ Sem_4/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+Console.Write("Введите число N : ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"{num} -> {Mult(num)} == ");
+MultPrint(num);
+
+int Mult(int number){
+    int _mult = 1;
+    for(int i = 1;i <= number;i++){
+        _mult *= i;
+    }
+    return _mult;
+}
+
+void MultPrint(int number){
+    for(int i = 1;i <= number;i++){
+        if(i == number){
+            Console.Write(i);
+        }
+        else{
+            Console.Write($"{i} * ");
+        }
+    }
+}
+
+
+
+
