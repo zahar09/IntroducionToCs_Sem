895ab5734f3118b087073ad7aab71ce08f392587
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-03 11:02:21 +0300

    Решил задачу 1

---
+++ Sem_4/task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"{num} -> {Numbers(num)}");
+
+int Numbers(int number){
+    int count = 0;
+    if(number == 0){
+        count = 1;
+    }
+    else{
+
+        while(number > 0){
+            number /= 10;
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
