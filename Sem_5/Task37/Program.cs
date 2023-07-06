f9fe45fd02b267f7c249687719ca3245574f06e8
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-06 12:09:57 +0300

    Решил задачу 37

---
+++ Sem_5/Task37/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 37:**Найдите произведение пар чисел в одномерном массиве.
+ Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+ Результат запишите в новом массиве.
+
+[1 2 3 4 5] -> 5 8 3
+
+[6 7 3 6]-> 36 21*/
+Console.Clear();
+int[] arrey = GetArr();
+int[] prouduct_of_numbers = GetProductOfNumbers(arrey);
+Console.Write($"{String.Join(", ",arrey)} -> {String.Join(", ",prouduct_of_numbers)}");
+
+int[] GetArr(){
+    int size = new Random().Next(2, 10);
+    int[] arr = new int[size];
+    for(int i = 0;i < arr.Length;i++){
+        arr[i] = new Random().Next(1, 20);
+    }
+    return arr;
+}
+
+int[] GetProductOfNumbers(int[] numbers){
+    int[] prouduct_of_numbers = new int[numbers.Length / 2];
+    for(int i = 0;i < numbers.Length / 2;i++){
+        prouduct_of_numbers[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+    }
+    return prouduct_of_numbers;
+}
